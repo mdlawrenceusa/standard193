02dfce2ead8e72074eca9a5df88196f8eea0a881
Michael Lawrence <mdlawrence@me.com>
2013-09-28 00:18:45 -0400

    straws
    config.autoload_paths += %W(#{config.root}/lib)

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+config.autoload_paths += %W(#{config.root}/lib)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
